f7509deeac1a759b30d1b7ec4bce194a77b5ae5e
Henry Cho <henrycho@mse-dev-13.app.byu.edu>
2021-09-21 15:27:18 -0600

    Literally completed this project

--- project7/welcome.php
+++ project7/welcome.php
@@ -1,14 +1,26 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
+
+$mysqli = new mysqli('localhost', 'root', 'root', 'Login');
+// Checking for connections
+if ($mysqli->connect_error) {
+    die('Connect Error (' . 
+    $mysqli->connect_errno . ') '. 
+    $mysqli->connect_error);
+}
+// SQL query to select data from database
+$sql = "SELECT * from users where username = '".$_SESSION['username']."'";
+$result = $mysqli->query($sql);
+$mysqli->close(); 
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,6 +33,17 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <body>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <?php 
+    while($row = mysqli_fetch_array($result))
+    { 
+        echo $row['first_name'];
+        echo "<br>";
+        echo $row['last_name'];
+        echo "<br>";
+        echo $row['birthday'];
+        echo "<br>";
+    }
+        ?>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
